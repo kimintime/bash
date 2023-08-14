3eebb450aaaacb9c5f6dc16e92d313654b43aa7b
Apollostowel <apollostowel@icloud.com>
2023-08-15 01:16:45 +0300

    Updating

---
+++ back.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#Param for number of directories to move back and count of directories
+max="$1"; 
+count=0
+
+#Loop through the number of moves and cd for each one
+for (( i= 0; i < $max; i++ )); do
+	((count=count+1))
+	cd ../
+done
+
+#Show how many directories moved and current directory
+echo "Moving back $count directories"
+echo "Present Directory: $(pwd)"
---
+++ fundamentals/params.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+echo "***********************************"
+
+echo "This is paramenter 1: $1"
+echo "This is paramenter 2: $2"
+echo "This is paramenter 3: $3"
+echo "This is paramenter 4: $4"
+echo "This is paramenter 5: $5"
+echo "This is paramenter 6: $6"
+
+#Double digit number parameters
+
+echo "This is paramenter 12: ${12}"
+
+#How many parameters
+
+echo " This is the number of parameters: $# "
+
+#Access all parameters
+
+echo "All parameters: $@"
+
+echo "***********************************"
+
+
+for i in $@; do
+	echo "$i"
+done
+
+echo "***********************************"
+
+# "$@" is different from "$*" - "$*" is considered just one element
+
+for i in "$*"; do
+	echo "$i"
+done
+
+
+
+
