91998031c3b44bef44f9be20f408a4acdbf31957
Apollostowel <apollostowel@icloud.com>
2023-08-27 00:14:02 +0300

    Updated cover_letter script

--- cover_letter.sh
+++ cover_letter.sh
@@ -2,30 +2,39 @@
 
 # Function to check that required text is not empty
 not_empty(){
-	local var_name="$1"
-  	local prompt_message="$2"
+  local var_name="$1"
+    local prompt_message="$2"
 
-	while true; do
-		read -p "$prompt_message: " input
+  while true; do
+    read -p "$prompt_message: " input
 
-    	if [ -n "$input" ]; then
-      		eval "$var_name=\"$input\"" # Assign input to the variable
-     		break
-    	else
-      		echo "Input cannot be empty."
-    	fi
+      if [ -n "$input" ]; then
+          eval "$var_name=\"$input\"" # Assign input to the variable
+        break
+      else
+          echo "Input cannot be empty."
+      fi
 
-	done
+  done
 }
 
-# Check if the user provided an output file name as an argument
-if [ $# -eq 0 ]; then
-  echo "Usage: $0 <output_file_name>"
-  exit 1
-fi
+# Function to remove optional text
+replace_optional() {
+  local var_name="$1"
+  local var_value="$2"
+  local template="$3"
 
+  if [ -n "$var_value" ]; then
+    echo "${template//\[$var_name\]/$var_value}"
+  else
+    echo "$template" # No replacement, keep the original template
+  fi
+}
+
+# Accept the filename as a param
 output_file="$1"
 
+# Create the output file. Adjust the path to suit your needs
 touch "$output_file"
 
 # Get the current date in the desired format (e.g., 21 August 2023)
@@ -37,8 +46,8 @@ echo "---------- ENTER ADDRESS ----------"
 read -p "Enter the company address [Enter to continue]: " company_address
 
 #Ask the user for the city, postal code
-read -p "Enter the city [Enter to continue]: " company_city
 read -p "Enter the postal code [Enter to continue]: " postal_code
+read -p "Enter the city [Enter to continue]: " company_city
 
 echo "---------- ENTER REPLACEMENT TEXT ----------"
 
@@ -49,13 +58,10 @@ read -p "Enter the replacement text for [employer's name] [Enter to continue]: "
 not_empty company_name "Enter the replacement text for [company name]: " 
 
 # Ask the user for position applied for
-not_empty postion "Enter the replacement text for [position]: "
+not_empty position "Enter the replacement text for [position]: "
 
 # Ask the user where they found the job posting
 not_empty job_posting "Enter the replacement text for [job posting]: "
-
-# Ask the user the job title
-not_empty job_title "Enter the replacement text for [title]: "
 
 # Ask the user the team being joined
 not_empty team "Enter the replacement text for [team]: "
@@ -66,47 +72,28 @@ not_empty vision "Enter the replacement text for [vision]: "
 
 echo "---------- GENERATING LETTER ----------"
 
-# Be sure to change the path of your input and output files to suit your needs.
 
-# Use sed to replace optional text, or delete the placeholders
-if [ -n "$employer_name" ]; then
-  sed "s/\[employer's name\]/$employer_name/g" input.txt > "$output_file"
-else
-  sed -i "s/Dear \[employer's name\]/To whom it may concern/g" "$output_file"
-fi
-
-if [ -n "$company_address" ]; then
-  sed "s/\[company address\]/$company_address/g" input.txt > "$output_file"
-else
-  sed "/\[company address\]/d" input.txt > "$output_file"
-fi
-
-if [ -n "$company_city" ]; then
-  sed "s/\[city\]/$company_city/g" input.txt > "$output_file"
-else
-  sed "/\[city\]/d" input.txt > "$output_file"
-fi
-
-if [ -n "$postal_code" ]; then
-  sed "s/\[postal code\]/$postal_code/g" input.txt > "$output_file"
-else
-  sed "/\[postal code\]/d" input.txt > "$output_file"
-fi
+# Read the cover letter template from the file. Adjust this to suit your needs
+cover_letter_template=$(<cover_letter.md)
 
 
-# Use sed to find and replace instances of [text in brackets]
-sed "s/\[today's date\]/$company_date/g" input.txt > "$output_file"
+# Replace instances of [text in brackets]
+cover_letter_content="${cover_letter_template//\[today date\]/$current_date}"
+cover_letter_content=$(replace_optional "employer's name" "$employer_name" "$cover_letter_content")
+cover_letter_content=$(replace_optional "company address" "$company_address" "$cover_letter_content")
+cover_letter_content=$(replace_optional "postal code" "$postal_code" "$cover_letter_content")
+cover_letter_content=$(replace_optional "city" "$company_city" "$cover_letter_content")
+cover_letter_content="${cover_letter_content//\[company name\]/$company_name}"
+cover_letter_content="${cover_letter_content//\[position\]/$position}"
+cover_letter_content="${cover_letter_content//\[job posting\]/$job_posting}"
+cover_letter_content="${cover_letter_content//\[team\]/$team}"
+cover_letter_content="${cover_letter_content//\[vision\]/$vision}"
 
-sed "s/\[company name\]/$company_name/g" input.txt > "$output_file"
 
-sed "s/\[position\]/$postion/g" input.txt > "$output_file"
+# Debug statement to check the replaced content
+echo "$cover_letter_content"
 
-sed "s/\[job posting\]/$job_posting/g" input.txt > "$output_file"
-
-sed "s/\[title\]/$job_title/g" input.txt > "$output_file"
-
-sed "s/\[team\]/$team/g" input.txt > "$output_file"
-
-sed "s/\[vision\]/$vision/g" input.txt > "$output_file"
+# Remember to change the path to suit your needs
+echo -e "$cover_letter_content" > "$output_file"
 
 echo "Replacement complete. Check '$output_file' for the updated text."
